5aec3895a1aa4dde9eb1cafb479427bf8fac4cf1
unknown <patrickhayes_6@hotmail.com>
2024-04-29 21:49:00 +1200

    added in our addToCart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,21 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
+    const { cartItems, cartQuantity } = useContext(CartContext);
+    console.log("cartographer items: ");
+    
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                <Button>GO TO CHECKOUT</Button>
+                {cartItems.map((cartItem) => (
+                    <CartItem key={cartItem.id} cartItem={cartItem} />
+                ))}
             </div>
+                <Button>GO TO CHECKOUT</Button>
         </div>
     )
 }
